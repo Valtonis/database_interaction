daefdd14439efa31114f497178bcef9b270b537f
Algu <alex.gusarin@gmail.com>
2020-10-23 09:09:06 +0300

    renaming

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
+from database_interaction_support import __version__
+
 setup(
-    name='database_interaction',
+    name='database_interaction_support',
     author="Alexander Gusarin",
     author_email="algu.remail@gmail.com",
     description="Classes to ease interaction with databases.",
-    version='0.0.1',
+    version=__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     python_requires='>=3.6',
