85d47a5fb4870fb38fc998db1cb31560064aeaa2
Algu <alex.gusarin@gmail.com>
2020-12-02 12:51:55 +0300

    making dist

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-from database_interaction_support import __version__
+from database_interaction import __version__
 
 setup(
-    name='database_interaction_support',
+    name='database_interaction',
     author="Alexander Gusarin",
     author_email="algu.remail@gmail.com",
     description="Classes to ease interaction with databases.",
     version=__version__,
-    packages=find_packages(),
+    packages=['database_interaction'],
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     python_requires='>=3.6',
 )
